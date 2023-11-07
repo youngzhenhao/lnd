835a984c4e0112af70d8acaff215fd699c50cdcd
Elle Mouton <elle.mouton@gmail.com>
2023-11-07 12:06:42 +0200

    htlcswitch: use ChannelUpdate interface in FetchLastChannelUpdate

--- htlcswitch/link_test.go
+++ htlcswitch/link_test.go
@@ -5878,7 +5878,7 @@ func TestForwardingAsymmetricTimeLockPolicies(t *testing.T) {
 // forwarding policy.
 func TestCheckHtlcForward(t *testing.T) {
 	fetchLastChannelUpdate := func(lnwire.ShortChannelID) (
-		*lnwire.ChannelUpdate1, error) {
+		lnwire.ChannelUpdate, error) {
 
 		return &lnwire.ChannelUpdate1{}, nil
 	}
