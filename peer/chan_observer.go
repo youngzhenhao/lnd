74279b02708f25b19247c55b5c1ccfbc51b18a77
Olaoluwa Osuntokun <laolu32@gmail.com>
2024-03-05 00:04:20 -0600

    fixup! peer: add an implementation of chancloser.ChanObserver

--- peer/chan_observer.go
+++ peer/chan_observer.go
@@ -1,6 +1,12 @@
 package peer
 
-import "github.com/btcsuite/btcd/wire"
+import (
+	"github.com/btcsuite/btcd/wire"
+	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/fn"
+	"github.com/lightningnetwork/lnd/htlcswitch"
+	"github.com/lightningnetwork/lnd/lnwallet/chancloser"
+)
 
 // channelView is a view into the current active/global channel state machine
 // for a given link.
@@ -11,21 +17,37 @@ type channelView interface {
 	// in the remote commit tx yet.
 	OweCommitment() bool
 
+	// IsChannelClean returns true if neither side has pending commitments,
+	// neither side has HTLC's, and all updates are locked in irrevocably.
+	// Internally, it
+	IsChannelClean() bool
+
 	// MarkCoopBroadcasted persistently marks that the channel close
 	// transaction has been broadcast.
 	MarkCoopBroadcasted(*wire.MsgTx, bool) error
+
+	// StateSnapshot returns a snapshot of the current fully committed
+	// state within the channel.
+	StateSnapshot() *channeldb.ChannelSnapshot
+
+	// MarkShutdownSent persists the given ShutdownInfo. The existence of
+	// the ShutdownInfo represents the fact that the Shutdown message has
+	// been sent by us and so should be re-sent on re-establish.
+	MarkShutdownSent(info *channeldb.ShutdownInfo) error
 }
 
 // linkController is capable of controlling the flow out incoming/outgoing
 // HTLCs to/from the link.
 type linkController interface {
-	// DisableAdds instructs the channel link to disable process new adds
-	// in the specified direction. An error is returned if the link is
-	// already disabled in that direction.
-	DisableAdds(outgoing bool) error
+	// DisableAdds sets the ChannelUpdateHandler state to allow
+	// UpdateAddHtlc's in the specified direction. It returns true if the
+	// state was changed and false if the desired state was already set
+	// before the method was called.
+	DisableAdds(outgoing bool) bool
 
-	// TODO(roasbeef): also use this to understand if link there or not for
-	// balance call?
+	// IsFlushing returns true when UpdateAddHtlc's are disabled in the
+	// direction of the argument.
+	IsFlushing(direction bool) bool
 }
 
 // chanObserver implements the chancloser.ChanObserver interface for the
@@ -61,7 +83,8 @@ func (l *chanObserver) DisableIncomingAdds() error {
 		return nil
 	}
 
-	return l.link.DisableAdds(false)
+	_ = l.link.DisableAdds(false)
+	return nil
 }
 
 // DisableOutgoingAdds instructs the channel link to disable process new
@@ -72,7 +95,8 @@ func (l *chanObserver) DisableOutgoingAdds() error {
 		return nil
 	}
 
-	return l.link.DisableAdds(true)
+	_ = l.link.DisableAdds(true)
+	return nil
 }
 
 // MarkCoopBroadcasted persistently marks that the channel close transaction
@@ -80,3 +104,51 @@ func (l *chanObserver) DisableOutgoingAdds() error {
 func (l *chanObserver) MarkCoopBroadcasted(tx *wire.MsgTx, local bool) error {
 	return l.chanView.MarkCoopBroadcasted(tx, local)
 }
+
+// MarkShutdownSent persists the given ShutdownInfo. The existence of the
+// ShutdownInfo represents the fact that the Shutdown message has been sent by
+// us and so should be re-sent on re-establish.
+func (l *chanObserver) MarkShutdownSent(deliveryAddr []byte,
+	isInitiator bool) error {
+
+	shutdownInfo := channeldb.NewShutdownInfo(deliveryAddr, isInitiator)
+	return l.chanView.MarkShutdownSent(shutdownInfo)
+}
+
+// FinalBalances is the balances of the channel once it has been flushed. If
+// Some, then this indicates that the channel is now in a state where it's
+// always flushed, so we can accelerate the state transitions.
+func (l *chanObserver) FinalBalances() fn.Option[chancloser.ShutdownBalances] {
+
+	chanClean := l.chanView.IsChannelClean()
+
+	switch {
+	// If we have a link, then the balances are final if both the incoming
+	// and outgoing adds are disabled _and_ the channel is clean.
+	case l.link != nil && l.link.IsFlushing(htlcswitch.Incoming) &&
+		l.link.IsFlushing(htlcswitch.Outgoing) && chanClean:
+
+		fallthrough
+
+	// If we don't have a link, then this is a restart case, so the
+	// balances are final.
+	case l.link == nil:
+		snapshot := l.chanView.StateSnapshot()
+
+		return fn.Some[chancloser.ShutdownBalances](
+			chancloser.ShutdownBalances{
+				LocalBalance:  snapshot.LocalBalance,
+				RemoteBalance: snapshot.RemoteBalance,
+			},
+		)
+
+	// Otherwise, the link is still active and not flushed, so the balances
+	// aren't yet final.
+	default:
+		return fn.None[chancloser.ShutdownBalances]()
+	}
+}
+
+// A compile-time assertion to ensure that chanObserver meets the
+// chancloser.ChanStateObserver interface.
+var _ chancloser.ChanStateObserver = (*chanObserver)(nil)
